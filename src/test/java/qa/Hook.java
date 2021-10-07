b915ab74d983c6d76a5536d3b9d8acaeebe993ad
mehmet-melek <mehmetmelekk@gmail.com>
2021-10-07 17:30:35 +0300

    Hook and Pom edited

--- src/test/java/qa/Hook.java
+++ src/test/java/qa/Hook.java
@@ -6,7 +6,6 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import qa.step_definitions.DriverSetup;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,14 +15,14 @@ public class Hook {
     @After(order = 1)
     public void afterScenario(Scenario scenario) throws IOException {
         if (scenario.isFailed()) {
-
-            File file  = ((TakesScreenshot)DriverSetup.getDriver()).getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(file, new File(System.getProperty("screenshot.rel.path")+"/Screenshot.png"));
-            byte[] data = Files.readAllBytes(Path.of(System.getProperty("screenshot.rel.path")+"/Screenshot.png"));
+            File file = ((TakesScreenshot) DriverSetup.getDriver()).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(file, new File(System.getProperty("screenshot.rel.path") + "/Screenshot.png"));
+            byte[] data = Files.readAllBytes(Path.of(System.getProperty("screenshot.rel.path") + "/Screenshot.png"));
             scenario.attach(data, "image/png", "My screenshot");
-            scenario.log("error log here");
         }
         DriverSetup.getDriver().closeApp();
         DriverSetup.getDriver().launchApp();
     }
+
+
 }
