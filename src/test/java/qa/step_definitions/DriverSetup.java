7472b38dd0f61bf0b75de834c38251a5fb938b29
mehmet-melek <mehmetmelekk@gmail.com>
2022-02-28 17:50:38 +0300

    Remote repository added and android node created

--- src/test/java/qa/step_definitions/DriverSetup.java
+++ src/test/java/qa/step_definitions/DriverSetup.java
@@ -3,6 +3,8 @@ package qa.step_definitions;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,13 +19,15 @@ public class DriverSetup {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("platformVersion", "12");
-        capabilities.setCapability("deviceName", "Pixel_3a_API_31_arm64-v8a");
+        capabilities.setCapability("deviceName", "Pixel_3a");
         capabilities.setCapability("app", System.getProperty("user.dir")+"/src/test/resources/app/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
         capabilities.setCapability("appPackage", "com.swaglabsmobileapp");
         capabilities.setCapability("appActivity", "com.swaglabsmobileapp.SplashActivity");
         capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("systemPort","8201");
         //capabilities.setCapability("avd", "Pixel_3a_API_31_arm64-v8a");
-        driver = new AppiumDriver<>(new URL("http://0.0.0.0:4723/wd/hub"),capabilities);
+        driver = new AppiumDriver<>(new URL("http://127.0.0.1:4444/wd/hub"),capabilities);
+        //driver = new AppiumDriver<>(new URL("http://0.0.0.0:4723/wd/hub"),capabilities);
         // capabilities.setCapability("ensureWebviewsHavePages", true);
     }
 }
